31e23dc2426e82369783d8ab5ef3a36423267ca2
Sam S <>
2019-05-09 16:55:05 -0600

    refactored to add axios on endpoints

--- backend_NBA/controllers/nbaDataController.js
+++ backend_NBA/controllers/nbaDataController.js
@@ -1,21 +1,42 @@
+const axios = require('axios');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
 
+
 router.use(bodyParser.json());
 
-const { execSync } = require('child_process');
+// const { execSync } = require('child_process');
 
 router.get('/', function (req, res) {
-  let data = execSync('./getData.sh');
-  res.send(data);
+  axios
+  .get(`http://data.nba.net/10s/prod/v1/2018/players.json`)
+  .then(response => {
+    res.send(response.data);
+  })
+    .catch(err => console.log(err));
 })
+
 
 router.get('/teams', function (req, res) {
-  let data = execSync('./getTeamData.sh');
-  res.send(data);
+  axios
+  .get(`http://data.nba.net/10s/prod/v2/2018/teams.json`)
+  .then(response => {
+    res.send(response.data);
+  })
+    .catch(err => console.log(err));
 })
 
+router.post('/player', function (req, res) {
+  console.log(req.body.id);
+  axios
+  .get(`http://data.nba.net/10s/prod/v1/2018/players/${req.body.id}_profile.json`)
+  .then(response => {
+    res.send(response.data);
+  })
+    .catch(err => console.log(err));
+})
 
 module.exports = router;
 
